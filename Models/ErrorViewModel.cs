56889ac929fbb9e17ba0ab30fcfcc60a84d6fd83
trieu.le <trieu.le@HCM-IT-TRIEULE>
2024-02-29 17:59:32 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EWM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
